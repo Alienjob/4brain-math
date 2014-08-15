c06a1638528cdf4c392dd7a0436746c8f8282f5e
Alien_job <Alien_job@list.ru>
2014-08-15 12:46:36 +0600

    Формирование поля "задача" скриптом.

--- Zadacha/public_html/js/Zadacha.js
+++ Zadacha/public_html/js/Zadacha.js
@@ -1,8 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 function HelloWorld() {
     
     return 'Hello, World!';
@@ -10,28 +5,39 @@ function HelloWorld() {
 }
 
 function Zadacha_Header(){
-    return '<h3>Задача 1</h3> <div class="zadacha"> <table>';
+    result = '<style> input{border:white; width:50px; height:20px;} .out-botton{color:green; text-align:center; padding:10px; cursor: pointer;} table{width:100%;} </style>';
+    return result + '<h3>Задача 1</h3> <div class="zadacha">';
+}
+
+function Zadacha_Question(text){
+    result = ' <table><tr ><td style="padding: 5px 10px 5px 5px; ">';
+    result += text;
+    result += '</td><td style="padding: 5px 10px 5px 5px;">Ваш ответ:<input type="text"/></td></tr> </table>';
+    return result;
+}
+function Zadacha_Answer(text){
+    result = '<div class="out-botton" onclick="out_botton_click()">Проверьте свое решение →</div> <div style="display: none;  color:DimGray; margin: 10px; border-top:2px groove silver;"><b>Ответ: ';
+    result += text;
+    result += '. </b></div>';
+    return result;
+}
+
+function Zadacha_End(Comment_text){
+    result = Comment_text;
+    result += '</div>';
+    return result;
+}
+function out_botton_click(){
+    console.log('ouch');
+    //$(this).next().slideToggle("fast");
 }
 
 function Zadacha(){
-    document.write(Zadacha_Header());
-    document.write(Zadacha_Header());
+    result =  Zadacha_Header();
+    result += Zadacha_Question('Возьмите 3000. Прибавьте 30. Прибавьте еще 2000. Добавьте еще 10. Плюс 2000. Добавьте еще 20. Плюс 1000. И плюс 30. Плюс 1000. И плюс 10.');
+    result += Zadacha_Answer('9 100');
+    result += Zadacha_End('Если вы решили задачу правильно и быстро, то вы смогли сконцентрироваться на цифрах и избежали соблазна получить красивый ответ. Именно такой подход нужен для устного счета.');
+    return result;
 }
-<tr ><td style="padding: 5px 10px 5px 5px; ">Возьмите 3000. Прибавьте 30. Прибавьте еще 2000. Добавьте еще 10. Плюс 2000. Добавьте еще 20. Плюс 1000. И плюс 30. Плюс 1000. И плюс 10.</td><td style="padding: 5px 10px 5px 5px;">Ваш ответ:<input type="text"/></td></tr>
-</table>
-<div class="out-botton">Проверьте свое решение →</div>
-<div style="display: none;  color:DimGray; margin: 10px; border-top:2px groove silver;"><b>Ответ: 9 100. </b>Если вы решили задачу правильно и быстро, то вы смогли сконцентрироваться на цифрах и избежали соблазна получить красивый ответ. Именно такой подход нужен для устного счета.</div>
-</div>
 
-<p>Попробуйте решить еще и другие похожие задачи на тренировку вычитания, деления и умножения в уме.</p>
-<h3>Задачи на внимание</h3>
-<div class="zadacha">
-<table>
-<tr ><td style="padding: 5px 10px 5px 5px; border-bottom: 1px solid grey;">3000 – 700 - 60 – 500 - 40 – 300 -20 – 100</td><td style="padding: 5px 10px 5px 5px; border-bottom: 1px solid grey;">Ваш ответ: <input type="text"/></td></tr>
-<tr ><td style="padding: 5px 10px 5px 5px; border-bottom: 1px solid grey;">1*2*3*4*3*2*1</td><td style="padding: 5px 10px 5px 5px; border-bottom: 1px solid grey;">Ваш ответ: <input type="text"/></td></tr>
-<tr ><td style="padding: 5px 10px 5px 5px; border-bottom: 1px solid grey;">100:2:2*3*2 + 50 – 100 + 200 – 30</td><td style="padding: 5px 10px 5px 5px; border-bottom: 1px solid grey;">Ваш ответ: <input type="text"/></td></tr>
-<tr ><td style="padding: 5px 10px 5px 5px; ">26+88+13+19</td><td style="padding: 5px 10px 5px 5px; ">Ваш ответ: <input type="text"/></td></tr>
-</table>
-<div class="out-botton">Проверьте свое решение →</div>
-<div style="display: none; color:DimGray; margin: 10px; border-top:2px groove silver;"><b>Ответы:</b> 1280, 144, 270, 146</div>
-</div>
+
