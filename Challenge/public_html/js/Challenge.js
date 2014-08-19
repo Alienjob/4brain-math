30fd8cf1c12ab1aed8dffe683019e988409ee228
Alien_job <Alien_job@list.ru>
2014-08-19 14:50:40 +0600

    Написан корневой класс - MathChallenge

--- Challenge/public_html/js/Challenge.js
+++ Challenge/public_html/js/Challenge.js
@@ -1,30 +1,118 @@
-function Challenge() {
 
+//служебные функции
+var readyList = [];
+function onReady(handler) {
+
+	if (!readyList.length) {
+		bindReady(function() {
+			for(var i=0; i<readyList.length; i++) {
+				readyList[i]();
+			}
+		});
+	}
+
+	readyList.push(handler);
+}
+function addEvent(elem, evType, fn) {
+	if (elem.addEventListener) {
+		elem.addEventListener(evType, fn, false);
+	}
+	else if (elem.attachEvent) {
+		elem.attachEvent('on' + evType, fn)
+	}
+	else {
+		elem['on' + evType] = fn
+	}
+}
+function bindReady(handler){
+
+	var called = false;
+
+	function ready() { // (1)
+		if (called) return;
+		called = true;
+		handler();
+	}
+
+	if ( document.addEventListener ) { // (2)
+		document.addEventListener( "DOMContentLoaded", function(){
+			ready();
+		}, false );
+	} else if ( document.attachEvent ) {  // (3)
+
+		// (3.1)
+		if ( document.documentElement.doScroll && window == window.top ) {
+			function tryScroll(){
+				if (called) return;
+				if (!document.body) return;
+				try {
+					document.documentElement.doScroll("left");
+					ready();
+				} catch(e) {
+					setTimeout(tryScroll, 0);
+				}
+			}
+			tryScroll();
+		}
+
+		// (3.2)
+		document.attachEvent("onreadystatechange", function(){
+
+			if ( document.readyState === "complete" ) {
+				ready();
+			}
+		});
+	}
+
+	// (4)
+    if (window.addEventListener)
+        window.addEventListener('load', ready, false);
+    else if (window.attachEvent)
+        window.attachEvent('onload', ready);
+    /*  else  // (4.1)
+        window.onload=ready
+	*/
+}
+
+//протокласс
+function Challenge() {
+    
+}
+
+//корневой класс для устного счета
+function MathChallenge() {
+    
+    var Combo = 0;
+    var Points = 0;
+    var Header = "Устный счет";
+    var OldQuestion= "Здесь будет показан предыдущий вопрос.";
+    var UID = createUUID();
+    var Question = expression(0,0,'+');
+    
+    var elQuestion;
+    var elIn;
+    var elOldQuestion;
+ 
+    
     function isNumber(n) {
       return !isNaN(parseInt(n)) && isFinite(n);
-    }
-
+    };
     function getRandomInt(min, max){
         return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    function createUUID() {
-        var s = [];
-        var hexDigits = "0123456789abcdef";
+    };
+    function createUUID()  {
+        var s = [];      
+        var hexDigits = "qwrtyuiopgabcdef";
         for (var i = 0; i < 36; i++) {
             s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
         }
-        s[14] = "4";  
-        s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  
-        s[8] = s[13] = s[18] = s[23] = "-";
+        //s[14] = "4";  
+        //s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  
+        //s[8] = s[13] = s[18] = s[23] = "-";
 
         var uuid = s.join("");
         return uuid;
-    }
-}
-    
-function MathChallenge() {
-    
-    MathChallenge.prototype = Challenge();
+    };
     
     //древовидная структура мат. выражения
     function expression(inFirstOperand, inSecondOperand, inOperator) {
@@ -34,48 +122,41 @@ function MathChallenge() {
         
         this.toString = function(){
             
-          return firstOperand.toString() + ' ' + operator + ' ' + secondOperand.toString();
+          return this.firstOperand.toString() + ' ' + this.operator + ' ' + this.secondOperand.toString();
           
         };
         this.call = function(){
             var o1 = 0;
             var o2 = 0;
             
-            if ( prototype.isNumber(firstOperand))
-                o1 = parseInt(firstOperand);
+            if ( isNumber(this.firstOperand))
+                o1 = parseInt(this.firstOperand);
             else 
-                o1 = firstOperand.call();
+                o1 = this.firstOperand.call();
             
-            if ( prototype.isNumber(secondOperand))
-                o1 = parseInt(secondOperand);
+            if ( isNumber(this.secondOperand))
+                o2 = parseInt(this.secondOperand);
             else 
-                o1 = secondOperand.call();
+                o2 = this.secondOperand.call();
             
-            if (operator === '+')
+            if (this.operator === '+')
                 return o1 + o2;
-            if (operator === '*')
+            if (this.operator === '*')
                 return o1 * o2;
-            if (operator === '-')
+            if (this.operator === '-')
                 return o1 - o2;
-            if (operator === '/')
+            if (this.operator === '/')
                 return Math.floor(o1 / o2);
             
         };
         
-    }
-    
-    //public
-    this.Combo = 0;
-    this.Points = 0;
-    this.Header = "Устный счет";
-    this.OldQuestion= "Здесь будет показан предыдущий вопрос.";
-    this.UID = this.createUUID();
-    
+    };
     function getRandomQuestion(){
         
-        return getPrimitiveQuestion({MIN : 1, MAX : this.prototype.getRandomInt(5, 1000)}, {MIN : 1, MAX : this.prototype.getRandomInt(5, 1000)}, '+-*/');
+        Question = getPrimitiveQuestion({MIN : 1, MAX : getRandomInt(5, 1000)}, {MIN : 1, MAX : getRandomInt(5, 1000)}, '+-*/');
+        return Question;
 
-    }
+    };
 
     function getPrimitiveQuestion(firstOperandLimit, secondOperandLimit, operatorLimit){
         
@@ -90,42 +171,54 @@ function MathChallenge() {
         
         return new expression(firstOperand, secondOperand, operator);
         
-    }
+    };
 
     function getHeader(){
         var result = '<style> input{border:white; width:50px; height:20px;} .out-botton{color:green; text-align:center; padding:10px; cursor: pointer;} table{width:100%;} </style>';
-        return result + '<h3>' + this.Header + '</h3> <div class="MathChallenge">';
-    }
+        return result + '<h3>' + Header + '</h3> <div class="MathChallenge">';
+    };
 
     function getQuestion(text){
         var result = ' <table><tr ><td style="padding: 5px 10px 5px 5px; ">';
-        result += text;
-        result += '</td><td style="padding: 5px 10px 5px 5px;">Ваш ответ:<input type="text"/></td></tr> </table>';
+        result += '<div  id = ' + 'Question_' + UID + '>' + text + '</div>';
+        result += '</td><td style="padding: 5px 10px 5px 5px;">Ваш ответ:<input id = "' + 'In_' + UID + '" type="text"/></td></tr> </table>';
         return result;
-    }
+    };
 
     function getEnd(Comment_text){
-        var result = Comment_text;
+        var result = '<div id = "' + 'OldQuestion_' + UID + '">' + Comment_text + '</div>';
         result += '</div>';
         return result;
+    };
+
+    function refresh(){
+        elOldQuestion.innerHTML = Question.toString() + " = " + Question.call() + " / ваш ответ " + elIn.value + ".";
+        this.Question = getRandomQuestion();
+        elQuestion.innerHTML = Question.toString();
+        elIn.value = "";
+        
+        //window.activeElement = elIn;
     }
+    
+    function findElements(){
+        elQuestion      = document.getElementById( 'Question_'    + UID);
+        elIn            = document.getElementById( 'In_'          + UID);
+        elOldQuestion   = document.getElementById( 'OldQuestion_' + UID);
+        
+        elIn.onchange = refresh;
+    };
 
     this.RandomText = function() {
         var randomQuestion = getRandomQuestion();
         var result =  getHeader();
         result += getQuestion(randomQuestion.toString());
-        result += getEnd('');
+        result += getEnd(OldQuestion);
         return result;
     };
     
-}
-
-
-
-
-function HelloWorld() {
-   return 'Hello, World!';
-}
+    onReady(findElements);
+    
+};
 
 
 
